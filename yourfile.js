9f1baba175f4f7e1f295769cebc2d9bad6c3780c
adamadam3d <adam.adam7d@gmail.com>
2022-06-28 19:10:09 +0200

    fading implanted

--- yourfile.js
+++ yourfile.js
@@ -7,6 +7,7 @@ let size = 16;
 newGame(size)
 const grids = document.querySelectorAll('.grid')
 container.appendChild(btn)
+grids.forEach((grid)=>grid.addEventListener("mouseover",()=>grid.style.backgroundColor='black'))
 
 
 const colour = document.createElement('button')
@@ -19,10 +20,12 @@ container.appendChild(colour)
 const fading = document.createElement('button')
 fading.textContent = 'Fading'
 fading.setAttribute('style','transform: translate(50%);')
+fading.addEventListener('click',bla)
 container.appendChild(fading)
 
 
 function newGame(grid) {
+    
     container.innerHTML='';
     for (let i =1;i<=grid;i++){
         const row = document.createElement('div')
@@ -35,10 +38,11 @@ function newGame(grid) {
             grid.classList.add('grid')
             row.appendChild(grid)
             grid.setAttribute('id',`g${x}`)
-            grid.addEventListener("mouseover",()=>grid.style.backgroundColor='black')
+            
     
         }
     }
+    
 }
 let i = 0;
 
@@ -46,15 +50,22 @@ let i = 0;
 function bla(){
 
     this.setAttribute('style',`filter: brightness(${100-i}%);`)
+    const grids = document.querySelectorAll('.grid')
+    grids.forEach((grid)=>grid.addEventListener("mouseover",()=>{
+        grid.setAttribute('style',`filter: brightness(${100-i}%);`);
+        i+=1
+    }));
+    fading.setAttribute('style','transform: translate(50%);')
+    container.appendChild(fading)
+    
 
-
-    i+=1;
 
 }
 
 function ran(){
     let col = Math.floor(Math.random()*16777215).toString(16);
     this.style.backgroundColor ="#" + col;
+    const grids = document.querySelectorAll('.grid')
     grids.forEach((grid)=>grid.addEventListener("mouseover",ran))
 }
 function click(){
@@ -67,6 +78,10 @@ function click(){
     container.appendChild(fading)
     container.appendChild(colour)
     i =0;
+    const grids = document.querySelectorAll('.grid')
+
+    grids.forEach((grid)=>grid.addEventListener("mouseover",()=>grid.style.backgroundColor='black'))
+
 
 }
 btn.addEventListener('click',click)
